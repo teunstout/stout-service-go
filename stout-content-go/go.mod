cb1c3cbcfdc59cb8b733a2b8ff35769e997a89aa
Teun Stout <60847859+teunstout@users.noreply.github.com>
2025-01-27 14:35:10 +0100

    Content service serving pdf

---
+++ stout-content-go/go.mod
@@ -0,0 +1,3 @@
+module stout.dev/content
+
+go 1.23.3
---
+++ stout-content-go/server.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+
+const CV_PATH = "./assets/Teun-Johán-Stout.pdf"
+
+func main() {
+	http.HandleFunc("/content/cv", handleDownloadCv)
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func handleDownloadCv(w http.ResponseWriter, r *http.Request) {
+	f, err := os.Open(CV_PATH)
+	if err != nil {
+		fmt.Println(err)
+		w.WriteHeader(500)
+		return
+	}
+
+	defer f.Close()
+
+	//Set header
+	w.Header().Set("Content-type", "application/pdf")
+
+	//Stream to response
+	if _, err := io.Copy(w, f); err != nil {
+		fmt.Println(err)
+		w.WriteHeader(500)
+	}
+}
