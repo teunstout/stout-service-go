ea08d3eb4eca7d58d709432e97572d39ec06c613
teunstout <teunstout@hotmail.com>
2025-02-23 18:10:08 +0100

    Commang to run docker container

--- run.sh
+++ run.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+docker run \
+    --network teunstout \
+    -e CONNECTION_STRING='user=golang password=golang host=http://postgres port=5432 dbname=development sslmode=disable' \
+    login-service
+
+
 cd ./authorization-service
 go build .
 docker build -t authorization-service .
